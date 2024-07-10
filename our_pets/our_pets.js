8e5c2656a486ad4f446931ef778d16a4dc8910af
demos-prog <73884833+demos-prog@users.noreply.github.com>
2024-07-10 13:47:16 +0300

    feat: pets start

---
+++ our_pets/our_pets.js
@@ -0,0 +1,47 @@
+// scroll
+const anchors = document.querySelectorAll('a.scroll-to')
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+
+    const blockID = anchor.getAttribute('href')
+    document.querySelector(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+})
+
+
+// modal window
+const modal = document.querySelector(".modal");
+const body = document.querySelector("body");
+const btns = document.querySelectorAll(".learnMoreBtn");
+const closeBtns = document.querySelectorAll(".close-button");
+
+btns.forEach((btn) => {
+  btn.onclick = function () {
+    body.style.overflow = "hidden";
+    document.querySelector('#modal-name').textContent = this.dataset.name;
+    document.querySelector('#modal-breed').textContent = this.dataset.breed;
+    document.querySelector('#modal-description').textContent = this.dataset.description;
+    document.querySelector('#modal-photo').setAttribute('src', `${this.dataset.photo}`);
+
+    modal.style.display = "block";
+  }
+})
+
+closeBtns.forEach((closeBtn) => {
+  closeBtn.onclick = function () {
+    modal.style.display = "none";
+    body.style.overflow = "auto";
+  }
+})
+
+window.onclick = function (event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+    body.style.overflow = "auto";
+  }
+}
